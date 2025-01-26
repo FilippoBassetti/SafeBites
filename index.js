225710b8ec7cd6b7ea3750b7d0ef35a32000f2d9
FilippoBassetti <filippo.bassetti@studenti.unitn.it>
2025-01-26 17:55:08 +0100

    some small adjustements

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('./app/app.js');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb+srv://filippobassetti:yn55_42N7ML47U6@cluster0sb.p4q6k.mongodb.net/Safebites?retryWrites=true&w=majority')
+/*
+mongoose.connect('mongodb+srv://filippobassetti:password@cluster0sb.p4q6k.mongodb.net/Safebites?retryWrites=true&w=majority')
   .then(() => {
     console.log('Connected to Database');
     app.listen(port, () => {
@@ -14,18 +15,22 @@ mongoose.connect('mongodb+srv://filippobassetti:yn55_42N7ML47U6@cluster0sb.p4q6k
   .catch((err) => {
     console.error('Database connection error:', err.message);
   });
+  */
 
-/*
-app.locals.db = mongoose.connect('mongodb+srv://filippobassetti:<yn55_42N7ML47U6>@cluster0sb.p4q6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0SB')
-.then ( () => {
-    
+
+app.locals.db = mongoose.connect(process.env.DB_URL)
+  .then(() => {
+
     console.log("Connected to Database");
-    
+
     app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
+      console.log(`Server listening on port ${port}`);
     });
-    
-});*/
+
+  }).catch((err) => {
+    console.error('Database connection error:', err.message);
+  });
+
 
 
 
